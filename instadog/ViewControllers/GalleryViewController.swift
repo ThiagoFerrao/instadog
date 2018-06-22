3eb277c33915ab30fc2e1dfe67fe95e779da608e
Thiago Cruz <thiago.ferrao91@gmail.com>
2018-06-22 07:25:45 -0300

    creates the GalleryVC and its components

---
+++ instadog/ViewControllers/GalleryViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GalleryViewController.swift
+//  instadog
+//
+//  Created by Thiago Ferrão on 22/06/18.
+//  Copyright © 2018 Thiago Ferrão. All rights reserved.
+//
+
+import UIKit
+
+class GalleryViewController: UIViewController {
+    
+}
