0930a9a550c88724d08aeb17078696013a83ea70
Thiago Cruz <thiago.ferrao91@gmail.com>
2018-06-21 22:25:08 -0300

    creates the project

---
+++ instadog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instadog
+//
+//  Created by Thiago Ferrão on 21/06/18.
+//  Copyright © 2018 Thiago Ferrão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
